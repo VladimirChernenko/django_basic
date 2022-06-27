b0a21d329b532ed6aeb368dccef8b6402eb4fa7e
Vladimir <chernenkovovan3012@gmail.com>
2022-06-27 12:02:35 +0300

    lesson_3

--- gb/gb/urls.py
+++ gb/gb/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('products_classic/', views.products, name='products_classic'),
     path('contact/', views.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
